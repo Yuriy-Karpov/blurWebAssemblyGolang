d7713c3dd468ed91e4e5c9d302b103491d07b735
Yuriy Karpov <karpov.yuriy.a@gmail.com>
2020-02-24 16:43:05 +0300

    add timer

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"syscall/js"
+	"time"
 )
 
 // Abs returns the absolute value of x.
@@ -13,39 +15,75 @@ func Abs(x int) int {
 	return x
 }
 
+type unitPixel struct {
+	target int
+	pixel  byte
+}
+
 func gaussMatrix(src []byte, width int, height int, radius int) []byte {
 
 	tableSize := 2*radius + 1
 	widthMax := width - 1
 	heightMax := height - 1
-
-	// fmt.Println(tableSize)
-	// fmt.Println(widthMax)
-	// fmt.Println(heightMax)
-
+	amountPixel := width * height
+	// fmt.Println("amountPixel", amountPixel)
+	jobs := make(chan unitPixel)
+	// jobs := make(chan int, amountPixel)
+	// done := make(chan bool)
 	// target := make([]byte, width*height)
+	offsetForY := 1
+	// var acmLoopCount int = 0
 	for y := 0; y < height; y++ {
 		indexHorizontLine := y * width
 		for indexPixelInLine := 0; indexPixelInLine < width; indexPixelInLine++ {
 			indexPixel := indexPixelInLine + indexHorizontLine
-			sumPixel := blurCore(src, indexPixel, indexPixelInLine, radius, widthMax, 1)
-			middleColorPixel := sumPixel / tableSize
-			src[indexPixel] = byte(middleColorPixel)
+			// acmLoopCount++
+			go blurCore(
+				src,
+				indexPixel,
+				indexPixelInLine,
+				radius,
+				widthMax,
+				offsetForY,
+				tableSize,
+				jobs)
 		}
 	}
-	offset := width
+	for i := 0; i < amountPixel; i++ {
+		a := <-jobs
+		src[a.target] = a.pixel
+	}
+	offsetForX := width
 	for x := 0; x < width; x++ {
 		for indexVertical := 0; indexVertical < height; indexVertical++ {
-			indexPixel := indexVertical*offset + x
-			sumPixel := blurCore(src, indexPixel, indexVertical, radius, heightMax, offset)
-			middleColorPixel := sumPixel / tableSize
-			src[indexPixel] = byte(middleColorPixel)
+			indexPixel := indexVertical*offsetForX + x
+			go blurCore(
+				src,
+				indexPixel,
+				indexVertical,
+				radius,
+				heightMax,
+				offsetForX,
+				tableSize,
+				jobs)
 		}
+	}
+	for i := 0; i < amountPixel; i++ {
+		a := <-jobs
+		src[a.target] = a.pixel
 	}
 	return src
 }
 
-func blurCore(src []byte, inIndex int, indexInLine int, radius int, max int, offset int) int {
+func blurCore(
+	src []byte,
+	inIndex int,
+	indexInLine int,
+	radius int,
+	max int,
+	offset int,
+	tableSize int,
+	jobs chan unitPixel) {
 	var tmpPixel int = 0
 	for r := -radius; r <= radius; r++ {
 		targetPixel := indexInLine + r
@@ -76,7 +114,9 @@ func blurCore(src []byte, inIndex int, indexInLine int, radius int, max int, off
 		}
 		tmpPixel += int(src[pxIndex])
 	}
-	return tmpPixel
+	tmpPixelBit := byte(tmpPixel / tableSize)
+	jobs <- unitPixel{inIndex, tmpPixelBit}
+	// fmt.Println("run", i)
 }
 
 func calcAsm(value js.Value, args []js.Value) interface{} {
@@ -88,10 +128,15 @@ func calcAsm(value js.Value, args []js.Value) interface{} {
 	radius := args[4].Int()
 
 	inBuf := make([]byte, width*height)
+	t1 := time.Now()
 	js.CopyBytesToGo(inBuf, src)
+	t := time.Now()
 	outBuf := gaussMatrix(inBuf, width, height, radius)
+	fmt.Println("timerGo:", time.Since(t))
 	js.CopyBytesToJS(target, outBuf)
+	fmt.Println("timerGoAll:", time.Since(t1))
 	callback.Invoke(js.Null(), target)
+
 	return nil
 }
 
